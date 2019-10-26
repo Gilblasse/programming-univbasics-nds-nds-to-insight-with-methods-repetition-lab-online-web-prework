5cc4b260d3d6b3084c792432523ab4d05af86490
Nethelbert <gilblasse@gmail.com>
2019-10-26 21:16:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -35,7 +35,10 @@ def list_of_directors(source)
 end
 
 def total_gross(source)
-  puts source
+  total = directors_totals(source)
+  directors = list_of_directors(source)
+  
+  puts total
   # Write this implementation
   #
   # Should use methods:
