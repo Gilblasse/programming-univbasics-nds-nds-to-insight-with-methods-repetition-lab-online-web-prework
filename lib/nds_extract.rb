589a9e8d6b4b891767d8063d8c262b67b16fede2
Nethelbert <gilblasse@gmail.com>
2019-10-26 21:32:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -39,8 +39,9 @@ def total_gross(source)
   director_array = list_of_directors(source)
   grand_total = 0
 
+  puts director_array[0]
   puts directors_gross.class
-  puts directors_gross["Stephen Spielberg"]
+  puts directors_gross[director_array[0]]
 =begin
   row_index = 0
   while row_index < director_array.count do
